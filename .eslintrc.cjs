73ca3046c1a55b03854b739216efbaf651db8ad5
Zakir Nuriiev <zakir.nuriiev@innervate.com>
2024-07-27 13:26:47 +0300

    add prettier, stylelint, husky with lint-staged

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,13 +9,10 @@ module.exports = {
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
+  settings: { react: { version: '18.3' } },
   plugins: ['react-refresh'],
   rules: {
     'react/jsx-no-target-blank': 'off',
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
   },
-}
+};
